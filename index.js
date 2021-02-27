69fcc2e59b89f33218b2e94cc2359dbc9a50dca4
Kirk An <you@example.comkirkan95@gmail.com>
2021-02-27 17:10:54 -0500

    init

---
+++ index.js
@@ -0,0 +1,25 @@
+$( document ).ready(function() {
+    $('#row-text').hide();
+    $('.emojicol').mouseenter( function(){
+        debugger
+        let unicode = getEmojiText($(this).text().codePointAt());
+        $('#row-text').text(unicode);
+        $('#row-text').addClass('d-flex');
+        $('#row-text').addClass('justify-content-center');
+        $('#row-text').fadeIn();
+    } ).mouseleave( function(){
+        $('#row-text').fadeOut();
+        setTimeout(function(){
+            $('#row-text').removeClass('d-flex');
+            $('#row-text').removeClass('justify-content-center');
+        },500)
+        
+    } );
+})
+
+function getEmojiText(input) {
+    switch (input){
+        case 10: return 'This is the whisky stuff. Apple does not like ice';
+    }
+    return 'blah blah blah';
+}
